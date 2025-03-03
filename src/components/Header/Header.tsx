744a7e2f760b51331a80765624511fcabfc4f301
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-03 11:08:31 +0200

    Added Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,80 @@
+import { AnimatePresence, motion } from 'motion/react';
+import { useState } from 'react';
+import { RiCloseLargeFill, RiMenu3Fill } from 'react-icons/ri';
+import {
+	ContactUs,
+	Container,
+	Logo,
+	Navigation,
+	useScreen,
+} from '../../shared';
+import MobileMenuModal from '../MobileMenuModal/MobileMenuModal';
+import css from './Header.module.css';
+
+const btnMenuAnimation = {
+	initial: { opacity: 0, y: 2 },
+	animate: { opacity: 1, y: 0 },
+	exit: { opacity: 0, y: 2 },
+	whileHover: { opacity: 0.8 },
+	whileFocus: { opacity: 0.8 },
+	transition: { duration: 0.2 },
+};
+
+export default function Header() {
+	const { isMobile } = useScreen();
+	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+	const openMenu = () => {
+		setIsMenuOpen(true);
+		document.body.style.overflow = 'hidden';
+	};
+
+	const closeMenu = () => {
+		setIsMenuOpen(false);
+		document.body.style.overflow = '';
+	};
+
+	return (
+		<>
+			<header className={css.header}>
+				<Container className={css.container}>
+					<button
+						type='button'
+						onClick={() => {
+							if (isMenuOpen) closeMenu();
+						}}
+					>
+						<Logo />
+					</button>
+					{isMobile ? (
+						<>
+							<Navigation className={css.list} />
+							<ContactUs />
+						</>
+					) : (
+						<button
+							type='button'
+							onClick={isMenuOpen ? closeMenu : openMenu}
+							className={css.btnMenu}
+						>
+							<AnimatePresence mode='wait'>
+								{isMenuOpen ? (
+									<motion.div key='close' {...btnMenuAnimation}>
+										<RiCloseLargeFill color='#3e3e42' size={24} />
+									</motion.div>
+								) : (
+									<motion.div key='menu' {...btnMenuAnimation}>
+										<RiMenu3Fill color='#3e3e42' size={24} />
+									</motion.div>
+								)}
+							</AnimatePresence>
+						</button>
+					)}
+				</Container>
+			</header>
+			{!isMobile && (
+				<MobileMenuModal handleToggle={closeMenu} isMenuOpen={isMenuOpen} />
+			)}
+		</>
+	);
+}

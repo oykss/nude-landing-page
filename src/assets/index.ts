a265116d62ac527b255dc3193ad499a06fa22a93
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-03 11:10:56 +0200

    Added pictures

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+// Store badges
+export { default as DownloadAppStore1x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@1x.png';
+export { default as DownloadAppStore2x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@2x.png';
+export { default as GetItOnGooglePlay1x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@1x.png';
+export { default as GetItOnGooglePlay2x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@2x.png';

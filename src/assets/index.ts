ea19280badc0a21bb181c5683a990f239a992e84
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-03 14:36:16 +0200

    Updated exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,22 @@
-// Store badges
-export { default as DownloadAppStore1x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@1x.png';
-export { default as DownloadAppStore2x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@2x.png';
-export { default as GetItOnGooglePlay1x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@1x.png';
-export { default as GetItOnGooglePlay2x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@2x.png';
+// store badges
+export { default as downloadAppStore1x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@1x.png';
+export { default as downloadAppStore2x } from './store-badges/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917@2x.png';
+export { default as getItOnGooglePlay1x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@1x.png';
+export { default as getItOnGooglePlay2x } from './store-badges/GetItOnGooglePlay_Badge_Web_color_English@2x.png';
+
+// Hero
+export { default as brand_1 } from './Hero/brand/brand-1.svg';
+export { default as brand_2 } from './Hero/brand/brand-2.svg';
+export { default as brand_3 } from './Hero/brand/brand-3.svg';
+export { default as brand_4 } from './Hero/brand/brand-4.svg';
+export { default as brand_5 } from './Hero/brand/brand-5.svg';
+export { default as brand_6 } from './Hero/brand/brand-6.svg';
+export { default as brand } from './Hero/brand/brand.svg';
+export { default as givingAway1x } from './Hero/giving-away@1x.png';
+export { default as givingAway2x } from './Hero/giving-away@2x.png';
+export { default as phone0_1x } from './Hero/phone@1x.png';
+export { default as phone0_2x } from './Hero/phone@2x.png';
+export { default as stain1x } from './Hero/stain@1x.png';
+export { default as stain2x } from './Hero/stain@2x.png';
+export { default as video1x } from './Hero/video@1x.png';
+export { default as video2x } from './Hero/video@2x.png';

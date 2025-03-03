8c6fb59025db5e382bba8d5a4512a0e7a74ffec5
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-03 11:12:34 +0200

    Main application added

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Header, Hero } from './components';
+
+import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { useStore } from './shared';
+
+export default function App() {
+	const { setIsMediaPoints } = useStore();
+	const isMobile = useMediaQuery({ query: '(min-width: 768px)' });
+	const isTablet = useMediaQuery({ query: '(min-width: 1280px)' });
+	const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
+
+	useEffect(() => {
+		setIsMediaPoints({ isMobile, isTablet, isDesktop });
+	}, [isMobile, isTablet, isDesktop, setIsMediaPoints]);
+
+	return (
+		<>
+			<Header />
+			<main>
+				<Hero />
+			</main>
+		</>
+	);
+}

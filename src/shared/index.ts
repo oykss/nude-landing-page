1436a596072b5c0710bba3a0ab5821ca83195441
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-03 11:07:50 +0200

    Added a common index for exports

---
+++ src/shared/index.ts
@@ -0,0 +1,20 @@
+// ui elements
+
+export { default as AppDownloadButtons } from './ui/AppDownloadButtons/AppDownloadButtons';
+export { default as ContactUs } from './ui/BtnContactUs/BtnContactUs';
+export { default as Container } from './ui/Container/Container';
+export { default as Logo } from './ui/Logo/Logo';
+export { default as Navigation } from './ui/Navigation/Navigation';
+export { default as SocList } from './ui/SocList/SocList';
+
+// styles
+export * from './styles/index.css';
+
+// constants
+export { default as BTN_ANIMATION } from './constants/animations';
+
+//store
+export { default as useStore } from './store/store';
+
+//hooks
+export { default as useScreen } from './hooks/useScreen';

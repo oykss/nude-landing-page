4b0b291690124c9716129305bf8026d74752d0f6
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-04 12:17:25 +0200

    Updated exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Benefits } from './Benefits/Benefits';
 export { default as Header } from './Header/Header';
 export { default as Hero } from './Hero/Hero';
 export { default as HowNudeHelps } from './HowNudeHelps/HowNudeHelps';
+export { default as InstantKeys } from './InstantKeys/InstantKeys';

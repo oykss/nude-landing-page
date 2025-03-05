f0289e37ae048dc07a40f252e571ecfaa23f51b9
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-05 21:21:55 +0200

    Fixed closing logic, updated animations

--- src/components/MobileMenuModal/MobileMenuModal.tsx
+++ src/components/MobileMenuModal/MobileMenuModal.tsx
@@ -1,58 +1,54 @@
 import { AnimatePresence, motion } from 'motion/react';
 import { MouseEventHandler } from 'react';
-import { ContactUs, Container, Navigation, SocList } from '../../shared';
+import {
+	ContactUs,
+	Container,
+	DOWN_ANIMATION,
+	FADE_ANIMATION,
+	Navigation,
+	SocList,
+} from '../../shared';
 import css from './MobileMenuModal.module.css';
 
 type PropsMobileMenuModal = {
-  isMenuOpen: boolean;
-  handleToggle: () => void;
-};
-
-const overlayAnimation = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-  transition: { duration: 0.2 },
-};
-
-const modalAnimation = {
-  initial: { y: '-100%' },
-  animate: { y: 0 },
-  exit: { y: '-100%' },
-  transition: { duration: 0.4 },
+	isMenuOpen: boolean;
+	handleToggle: () => void;
 };
 
 export default function MobileMenuModal({
-  isMenuOpen,
-  handleToggle,
+	isMenuOpen,
+	handleToggle,
 }: PropsMobileMenuModal) {
-  const handleClick: MouseEventHandler<HTMLDivElement> = e => {
-    const target = e.target as HTMLElement;
+	const handleClick: MouseEventHandler<HTMLDivElement> = e => {
+		const target = e.target as HTMLElement;
 
-    if (!target.closest('[data-modal]') || target.nodeName === 'A') {
-      handleToggle();
-    }
-  };
+		if (
+			!target.closest('[data-modal]') ||
+			['A', 'BUTTON'].includes(target.nodeName)
+		) {
+			handleToggle();
+		}
+	};
 
-  return (
-    <AnimatePresence initial={false}>
-      {isMenuOpen ? (
-        <motion.div
-          className={css.overlay}
-          onClick={handleClick}
-          {...overlayAnimation}
-        >
-          <motion.div className={css.modal} {...modalAnimation} data-modal>
-            <Container>
-              <Navigation className={css.list} />
-              <div className={css.wrapper}>
-                <SocList />
-                <ContactUs />
-              </div>
-            </Container>
-          </motion.div>
-        </motion.div>
-      ) : null}
-    </AnimatePresence>
-  );
+	return (
+		<AnimatePresence initial={false}>
+			{isMenuOpen ? (
+				<motion.div
+					className={css.overlay}
+					onClick={handleClick}
+					{...FADE_ANIMATION}
+				>
+					<motion.div className={css.modal} {...DOWN_ANIMATION} data-modal>
+						<Container>
+							<Navigation className={css.list} />
+							<div className={css.wrapper}>
+								<SocList />
+								<ContactUs />
+							</div>
+						</Container>
+					</motion.div>
+				</motion.div>
+			) : null}
+		</AnimatePresence>
+	);
 }

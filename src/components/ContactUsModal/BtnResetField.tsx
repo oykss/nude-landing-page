790ae8942d5d446bb2866a2627445376c61717db
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-05 21:23:51 +0200

    Added BtnResetField component to reset input field

---
+++ src/components/ContactUsModal/BtnResetField.tsx
@@ -0,0 +1,24 @@
+import { motion } from 'motion/react';
+import { RiCloseLargeFill } from 'react-icons/ri';
+import { FADE_ANIMATION } from '../../shared';
+
+type PropsBtnResetFieldProps = {
+	handleResetField: (field: string) => void;
+	className?: string;
+};
+
+export default function BtnResetField({
+	handleResetField,
+	className,
+}: PropsBtnResetFieldProps) {
+	return (
+		<motion.button
+			type='button'
+			className={className}
+			onClick={() => handleResetField('')}
+			{...FADE_ANIMATION}
+		>
+			<RiCloseLargeFill size={16} color='#374f4c' />
+		</motion.button>
+	);
+}

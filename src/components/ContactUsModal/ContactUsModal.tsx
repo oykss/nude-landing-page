e616c61751a74db8432ab543d374e45510a0fd1c
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-05 21:24:37 +0200

    Added ContactUsMenu component

---
+++ src/components/ContactUsModal/ContactUsModal.tsx
@@ -0,0 +1,51 @@
+import { AnimatePresence, motion } from 'motion/react';
+import { IoIosArrowBack } from 'react-icons/io';
+import {
+	BUTTON_PRIMARY_ANIMATION,
+	FADE_ANIMATION,
+	RIGHT_ANIMATION,
+	useScreen,
+	useStore,
+} from '../../shared';
+import ContactForm from './ContactForm/ContactForm';
+import css from './ContactUsModal.module.css';
+import PhonesSlider from './PhonesSlider/PhonesSlider';
+
+export default function ContactUsMenu() {
+	const { isOpen, closeModal } = useStore();
+	const { isMobile } = useScreen();
+
+	return (
+		<AnimatePresence initial={false}>
+			{isOpen ? (
+				<motion.div
+					className={css.overlay}
+					onClick={closeModal}
+					{...FADE_ANIMATION}
+				>
+					<div className={css.modal} onClick={e => e.stopPropagation()}>
+						<div className={css.formWrap}>
+							<motion.button
+								type='button'
+								onClick={closeModal}
+								className={css.btnBack}
+								{...BUTTON_PRIMARY_ANIMATION}
+								{...FADE_ANIMATION}
+								transition={{ delay: 0.4 }}
+							>
+								<IoIosArrowBack size={16} color='#3e3e42' />
+								Back
+							</motion.button>
+							<ContactForm />
+						</div>
+						{isMobile && (
+							<motion.div {...RIGHT_ANIMATION}>
+								<PhonesSlider />
+							</motion.div>
+						)}
+					</div>
+				</motion.div>
+			) : null}
+		</AnimatePresence>
+	);
+}

19f8ae375153377c8612b40178155490f576bf86
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-07 13:55:59 +0200

    Added animations for Hero and BrandList components, improved alignment of elements in BrandList

--- src/components/Hero/BrandList/BrandList.tsx
+++ src/components/Hero/BrandList/BrandList.tsx
@@ -1,13 +1,21 @@
-import BRAND_LIST from './constant';
+import { Animated } from '../../../shared';
 import css from './BrandList.module.css';
+import BRAND_LIST from './constant';
 
 export default function BrandList() {
 	return (
 		<ul className={css.list}>
 			{BRAND_LIST.map(({ icon, alt }, i) => (
-				<li key={i}>
+				<Animated
+					as='li'
+					key={i}
+					variants={{
+						hidden: { x: -50, opacity: 0 },
+						visible: { x: 0, opacity: 1, transition: { delay: 0.15 * i } },
+					}}
+				>
 					<img src={icon} alt={alt} />
-				</li>
+				</Animated>
 			))}
 		</ul>
 	);
--- src/components/Hero/HeroMedia/HeroMedia.tsx
+++ src/components/Hero/HeroMedia/HeroMedia.tsx
@@ -1,67 +1,83 @@
+import { AnimatePresence, motion } from 'motion/react';
 import {
 	givingAway1x,
 	givingAway2x,
 	phone0_1x,
 	phone0_2x,
 	stain1x,
-	stain2x,
 	video1x,
 	video2x,
 } from '../../../assets';
-import { useScreen } from '../../../shared';
+import { useMediaPoints } from '../../../shared';
 import css from './HeroMedia.module.css';
 
 export default function HeroMedia() {
-	const { isMobile } = useScreen();
+	const { isMobile } = useMediaPoints();
+
 	return (
-		<div className={css.wrap}>
-			{isMobile && (
-				<div className={css.imgWrap}>
-					<img
-						className={css.givingAway}
-						src={givingAway1x}
-						srcSet={`${givingAway1x} 1x, ${givingAway2x} 2x`}
-						alt='Download on the App Store'
-						width={240}
-						height={221}
-					/>
-					<div className={css.videoWrap}>
-						<img
-							className={css.stain}
-							src={stain1x}
-							srcSet={`${stain1x} 1x, ${stain2x} 2x`}
-							alt='Download on the App Store'
-							width={347}
-							height={364}
+		<AnimatePresence initial={true}>
+			<motion.div className={css.wrap} initial='hidden' animate='visible'>
+				{isMobile && (
+					<div className={css.imgWrap}>
+						<motion.img
+							className={css.givingAway}
+							src={givingAway1x}
+							srcSet={`${givingAway1x} 1x, ${givingAway2x} 2x`}
+							alt='Giving away promotion image'
+							width={240}
+							height={221}
+							variants={{
+								hidden: { y: -50, opacity: 0 },
+								visible: { y: 0, opacity: 1, transition: { delay: 0.2 } },
+							}}
 						/>
-						<a
-							href='https://youtu.be/eayImZxG-mk?si=E1iw98c85X7kTb7M'
-							target='_blank'
-							rel='noopener noreferrer'
+						<motion.div
+							className={css.videoWrap}
+							variants={{
+								hidden: { y: 50, opacity: 0 },
+								visible: { y: 0, opacity: 1, transition: { delay: 0.3 } },
+							}}
 						>
 							<img
-								className={css.video}
-								src={video1x}
-								srcSet={`${video1x} 1x, ${video2x} 2x`}
-								alt='Download on the App Store'
-								width={194}
-								height={152}
+								className={css.stain}
+								src={stain1x}
+								alt='Decorative stain background'
+								width={347}
+								height={364}
 							/>
-							<p>
-								Watch: Nude in <br /> under 2 mins
-							</p>
-						</a>
+							<a
+								href='https://youtu.be/eayImZxG-mk?si=E1iw98c85X7kTb7M'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<img
+									className={css.video}
+									src={video1x}
+									srcSet={`${video1x} 1x, ${video2x} 2x`}
+									alt='Video preview thumbnail'
+									width={194}
+									height={152}
+								/>
+								<p>
+									Watch: Nude in <br /> under 2 mins
+								</p>
+							</a>
+						</motion.div>
 					</div>
-				</div>
-			)}
-			<img
-				className={css.phone}
-				src={phone0_1x}
-				srcSet={`${phone0_1x} 1x, ${phone0_2x} 2x`}
-				alt='Download on the App Store'
-				width={289}
-				height={584}
-			/>
-		</div>
+				)}
+				<motion.img
+					className={css.phone}
+					src={phone0_1x}
+					srcSet={`${phone0_1x} 1x, ${phone0_2x} 2x`}
+					alt='Download on the App Store'
+					width={289}
+					height={584}
+					variants={{
+						hidden: { x: 50, opacity: 0 },
+						visible: { x: 0, opacity: 1, transition: { delay: 0.4 } },
+					}}
+				/>
+			</motion.div>
+		</AnimatePresence>
 	);
 }
